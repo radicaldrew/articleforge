a8a4a7bc9e8841a0623bd2d29c784485d55cb311
Andrew Karp <drewkarp@gmail.com>
2021-12-12 17:33:57 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# articleforge
+
+![npm](https://img.shields.io/npm/v/articleforge.svg) ![license](https://img.shields.io/npm/l/articleforge.svg) ![github-issues](https://img.shields.io/github/issues/radicaldrew/articleforge.svg)  ![Circle CI build status](https://circleci.com/gh/radicaldrew/articleforge.svg?style=svg)
+
+articleforge rest client sdk
+
+![nodei.co](https://nodei.co/npm/articleforge.png?downloads=true&downloadRank=true&stars=true)
+
+![travis-status](https://img.shields.io/travis/radicaldrew/articleforge.svg)
+![stars](https://img.shields.io/github/stars/radicaldrew/articleforge.svg)
+![forks](https://img.shields.io/github/forks/radicaldrew/articleforge.svg)
+
+![forks](https://img.shields.io/github/forks/radicaldrew/articleforge.svg)
+
+![](https://david-dm.org/radicaldrew/articleforge/status.svg)
+![](https://david-dm.org/radicaldrew/articleforge/dev-status.svg)
+
+## Features
+
+
+## Install
+
+`npm install --save articleforge`
+
+
+## Scripts
+
+ - **npm run build** : `rimraf dist && tsc`
+ - **npm run lint** : `gts lint`
+ - **npm run clean** : `gts clean`
+ - **npm run compile** : `tsc`
+ - **npm run fix** : `gts fix`
+ - **npm run readme** : `node ./node_modules/.bin/node-readme`
+
+## Dependencies
+
+Package | Version | Dev
+--- |:---:|:---:
+[axios](https://www.npmjs.com/package/axios) | ^0.24.0 | ✖
+[@types/node](https://www.npmjs.com/package/@types/node) | ^14.11.2 | ✔
+[gts](https://www.npmjs.com/package/gts) | ^3.1.0 | ✔
+[node-readme](https://www.npmjs.com/package/node-readme) | ^0.1.9 | ✔
+[rimraf](https://www.npmjs.com/package/rimraf) | ^3.0.2 | ✔
+[typescript](https://www.npmjs.com/package/typescript) | ^4.4.4 | ✔
+
+
+## Contributing
+
+Contributions welcome; Please submit all pull requests the against master branch. If your pull request contains JavaScript patches or features, you should include relevant unit tests. Please check the [Contributing Guidelines](contributng.md) for more details. Thanks!
+
+## Author
+
+Andrew Karp <undefined> http://github.com/radicaldrew
+
+## License
+
+ - **MIT** : http://opensource.org/licenses/MIT
---
+++ src/index.ts
@@ -0,0 +1,141 @@
+import axios from 'axios';
+
+interface ArticleParams {
+  keyword: string;
+  sub_keywords?: string;
+  length?: string;
+  title?: number;
+  image?: number;
+  video?: number;
+  auto_links?: string;
+  turing_spinner: number;
+  quality: number;
+  rewrite_num: number;
+}
+
+export class ArticleForge {
+  private key: string;
+  private baseurl: string;
+  private methods: Array<string>;
+  constructor(key: string) {
+    this.key = key;
+    this.methods = [
+      'check_usage',
+      'view_articles',
+      'view_article',
+      'view_spintax',
+      'view_spin',
+      'initiate_article',
+      'get_api_progress',
+      'get_api_article_result',
+      'delete_article',
+    ];
+    this.baseurl = 'https://af.articleforge.com/api/';
+  }
+  async request(method: string, params: Object) {
+    try {
+      if (!method) {
+        return {error: 'method name required'};
+      }
+      if (!this.methods.includes(method)) {
+        return {error: 'method not found'};
+      }
+      const response = await axios.post(`${this.baseurl}${method}`, {
+        key: this.key,
+        ...params,
+      });
+      return response.data;
+    } catch (error) {
+      return {error};
+    }
+  }
+  async usage() {
+    try {
+      return await this.request('check_usage', {});
+    } catch (error) {
+      return {error};
+    }
+  }
+  async articles() {
+    try {
+      return await this.request('view_articles', {});
+    } catch (error) {
+      return {error};
+    }
+  }
+  async article(article_id: string) {
+    try {
+      if (!article_id) {
+        return {error: 'article id is required'};
+      }
+      return await this.request('view_article', {
+        article_id,
+      });
+    } catch (error) {
+      return {error};
+    }
+  }
+  async create_article(params: ArticleParams) {
+    try {
+      if (!params) {
+        return {error: 'params are required'};
+      }
+
+      if (!params.keyword) {
+        return {error: 'keyword is required'};
+      }
+
+      return await this.request('initiate_article', {
+        keyword: params.keyword,
+        sub_keywords: params.sub_keywords ? params.sub_keywords : '',
+        length: params.keyword ? params.keyword : '',
+        title: params.title ? params.title : 0,
+        image: params.image ? params.image : 0.0,
+        video: params.video ? params.video : 0.0,
+        auto_links: params.auto_links ? params.auto_links : '',
+        turing_spinner: params.turing_spinner ? params.turing_spinner : 0,
+        quality: params.quality ? params.quality : 4,
+        rewrite_num: params.rewrite_num ? params.rewrite_num : 5,
+      });
+    } catch (error) {
+      return {error};
+    }
+  }
+
+  async progress(ref_key: string) {
+    try {
+      if (!ref_key) {
+        return {error: 'ref_key is required'};
+      }
+      return await this.request('get_api_progress', {
+        ref_key,
+      });
+    } catch (error) {
+      return {error};
+    }
+  }
+  async result(ref_key: string) {
+    try {
+      if (!ref_key) {
+        return {error: 'ref_key is required'};
+      }
+      return await this.request('get_api_article_result', {
+        ref_key,
+      });
+    } catch (error) {
+      return {error};
+    }
+  }
+  async delete(article_id: string) {
+    try {
+      if (!article_id) {
+        return {error: 'article id is required'};
+      }
+      return await this.request('delete_article', {
+        article_id,
+      });
+    } catch (error) {
+      return {error};
+    }
+  }
+}
